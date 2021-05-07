2f915275fdf10bf241ff352a7d681d7c1466377a
Dmitrii Mingalev <d.mingalev99@gmail.com>
2021-05-08 01:01:29 +0300

    init

---
+++ truffle-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    teams: {
+        url: "https://sandbox.truffleteams.com/795a754b-ea8c-43f7-9278-385581ba340a",
+      network_id: "1620083791470"
+    }
+  }
+};
