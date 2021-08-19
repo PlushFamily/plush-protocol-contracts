c8398e8ab21a96a81d368f79a28c0f0a0e3fd3d2
Dmitrii Mingalev <d.mingalev99@gmail.com>
2021-08-19 05:08:35 +0300

    add folder for old contracts

--- contracts/old/PlushCoreToken/PlushCoreToken_v1.sol
+++ contracts/old/PlushCoreToken/PlushCoreToken_v1.sol
@@ -3,46 +3,26 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract PlushCoreToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract PlushCoreTokenV1 is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("PlushCoreToken", "PLASH") {}
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://api.plush.dev/token/";
-    }
+    constructor() ERC721("PlushCoreToken", "PLUSH") {}
 
     function safeMint(address to) public onlyOwner {
         _safeMint(to, _tokenIdCounter.current());
         _tokenIdCounter.increment();
     }
 
-    // The following functions are overrides required by Solidity.
-
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 
     function supportsInterface(bytes4 interfaceId)
