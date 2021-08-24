3adc0171c6eca9fcfeb313ff49686e6c90b03a7a
Dmitrii Mingalev <d.mingalev99@gmail.com>
2021-08-24 05:50:46 +0300

    Test Plush Forest Token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,14 @@
 async function main() {
     // Grab the contract factory 
     const PlushCoreToken = await ethers.getContractFactory("PlushCoreToken");
+    const PlushForestToken = await ethers.getContractFactory("PlushForestToken");
     
     // Start deployment, returning a promise that resolves to a contract object
     const plushCoreToken = await PlushCoreToken.deploy(); // Instance of the contract 
     console.log("Contract deployed to address:", plushCoreToken.address);
+
+    const plushForestToken = await PlushForestToken.deploy(); // Instance of the contract
+    console.log("Contract deployed to address:", plushForestToken.address);
  }
  
  main()
