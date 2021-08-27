65b1aae7d7b5c543bce90e7da548b1ff46d260f3
Dmitrii Mingalev <d.mingalev99@gmail.com>
2021-08-27 13:47:50 +0300

    Fix token contract

--- contracts/PlushLogo.sol
+++ contracts/PlushLogo.sol
@@ -1,17 +1,28 @@
 // SPDX-License-Identifier: MIT
+// Plush Logo v1
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PlushLogo is ERC721URIStorage {
+contract PlushLogo is ERC721URIStorage, Ownable, Pausable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() ERC721("PlushLogo", "LOGO") {}
 
-    function mint(address to, string memory tokenURI)
-    public
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory tokenURI)
+    public onlyOwner
     returns (uint256)
     {
         _tokenIds.increment();
@@ -21,5 +32,13 @@ contract PlushLogo is ERC721URIStorage {
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    internal
+    whenNotPaused
+    override
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
