656ff8e7ef95558ae54153d26378734f9c1ca869
Dmitrii <d.mingalev99@gmail.com>
2021-10-27 23:40:20 +0300

    Add more things for contract

--- contracts/PlushGetTree.sol
+++ contracts/PlushGetTree.sol
@@ -9,14 +9,73 @@ contract PlushGetTree is Ownable {
     PlushForestToken plushForest;
     bool public isActive;
     address safeAddress;
-    uint256 mintPrice;
+    string[] trees;
+
+    mapping(string => Tree) treeMap;
+
+    struct Tree {
+        bool isValid;
+        string name;
+        uint256 price;
+        uint256 count;
+    }
 
     constructor(address _plushForestAddress, address _safeAddress)
     {
         isActive = true;
-        mintPrice = 0.001 ether;
         plushForest = PlushForestToken(_plushForestAddress);
         safeAddress = _safeAddress;
+    }
+
+    function addTreeType(string memory _type, uint256 _price, uint256 _count) external onlyOwner {
+        require(!treeMap[_type].isValid, 'This type of tree already exists');
+
+        trees.push(_type);
+        treeMap[_type] = Tree(true, _type, _price, _count);
+    }
+
+    function removeTreeType(string memory _type) external onlyOwner {
+        require(treeMap[_type].isValid, 'Not a valid tree type.');
+
+        uint256 index;
+
+        for (uint256 i = 0; i < trees.length; i++) {
+            if (keccak256(abi.encodePacked(trees[i])) == keccak256(abi.encodePacked(_type))) {
+                index = i;
+            }
+        }
+
+        trees[index] = trees[trees.length - 1];
+
+        trees.pop();
+        delete treeMap[_type];
+    }
+
+    function getTreeTypes() external view returns(string[] memory) {
+        return trees;
+    }
+
+    function getTreeTypeCount(string memory _type) external view returns(uint256) {
+        require(treeMap[_type].isValid, 'Not a valid tree type.');
+        return treeMap[_type].count;
+    }
+
+    function getTreeTypePrice(string memory _type) external view returns(uint256) {
+        require(treeMap[_type].isValid, 'Not a valid tree type.');
+        return treeMap[_type].price;
+    }
+
+    function setTreeTypePrice(string memory _type, uint256 _price) external onlyOwner {
+        require(isActive);
+        require(treeMap[_type].isValid, 'Not a valid tree type.');
+        require(_price > 0);
+        treeMap[_type].price = _price;
+    }
+
+    function setTreeTypeCount(string memory _type, uint256 _count) external onlyOwner {
+        require(isActive);
+        require(treeMap[_type].isValid, 'Not a valid tree type.');
+        treeMap[_type].count = _count;
     }
 
     function setSafeAddress(address _address) external onlyOwner {
@@ -27,20 +86,12 @@ contract PlushGetTree is Ownable {
         return safeAddress;
     }
 
-    function getMintPrice() external view returns(uint) {
-        return mintPrice;
-    }
-
-    function setMintPrice(uint256 _price) external onlyOwner {
+    function mint(address _mintAddress, string memory _type) public payable {
         require(isActive);
-        require(_price > 0);
-        mintPrice = _price;
-    }
-
-    function mint(address mintAddress)public payable {
-        require(isActive);
-        require(msg.value == mintPrice, "Minting fee");
-        plushForest.safeMint(mintAddress);
+        require(treeMap[_type].isValid, 'Not a valid tree type.');
+        require(uint256 (msg.value) == treeMap[_type].price, "Minting fee");
+        plushForest.safeMint(_mintAddress);
+        treeMap[_type].count = treeMap[_type].count - 1;
     }
 
     function changeContractStatus() public onlyOwner {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,13 +12,14 @@ async function main() {
    // const plushForestToken = await PlushForestToken.deploy(); // Instance of the contract
   //  console.log("Plush Forest Token -> deployed to address:", plushForestToken.address);
 
-    //const plushLogo = await PlushLogo.deploy(); // Instance of the contract
-    //console.log("Plush Logo -> deployed to address:", plushLogo.address);
+   //const plushLogo = await PlushLogo.deploy(); // Instance of the contract
+   // console.log("Plush Logo -> deployed to address:", plushLogo.address);
 
     const plushGetTree = await PlushGetTree.deploy(
         '0x831fbc2be13ce31884a59fa976f2588c08e4e23e', // Forest address
         '0xdB38a0a93a9AE52d505bBd0E55aBB40aBdfaC8b5' // withdrawal address, need to change
     ); // Instance of the contract
+    console.log("Plush GetTree -> hash:", plushGetTree.deployTransaction.hash);
     console.log("Plush GetTree -> deployed to address:", plushGetTree.address);
  }
  
