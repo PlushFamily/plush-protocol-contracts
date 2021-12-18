71bf33c4864dbae5fa1a04141d206ebfff59e4a8
Dmitrii <d.mingalev99@gmail.com>
2021-12-18 04:26:31 +0300

    add validate env file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,20 +11,34 @@ const {
   POLYGONSCAN_API_KEY,
 } = process.env;
 
-module.exports = {
+if (
+  !RINKEBY_API_URL ||
+  !MUMBAI_API_URL ||
+  !GOERLI_API_URL ||
+  !PRIVATE_KEY ||
+  !ETHERSCAN_API_KEY ||
+  !POLYGONSCAN_API_KEY
+) {
+  throw new Error('Not all variables are specified in the env file!');
+}
+
+export default {
   solidity: {
     compilers: [
+      {
+        version: '0.8.7',
+      },
       {
         version: '0.8.2',
       },
       {
-        version: '0.6.0',
+        version: '0.6.6',
       },
       {
         version: '0.6.2',
       },
       {
-        version: '0.6.6',
+        version: '0.6.0',
       },
     ],
     settings: {
