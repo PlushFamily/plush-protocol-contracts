a666febcbbebf5638d024f937e02ffb1f296c42a
Dmitrii <d.mingalev99@gmail.com>
2021-12-21 04:26:24 +0300

    ERC20 add burnable

--- contracts/PlushCoin.sol
+++ contracts/PlushCoin.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @custom:security-contact security@plush.family
-contract PlushCoin is ERC20 {
-    constructor() ERC20("Plush Coin", "PLSH") {
+contract PlushCoin is ERC20, ERC20Burnable {
+    constructor() ERC20("PlushCoin", "PLSH") {
         _mint(msg.sender, 10000000000 * 10 ** decimals());
     }
 }
