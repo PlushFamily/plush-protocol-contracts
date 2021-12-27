0fc4ab59fed548041a9e9082242f158a008fabd0
Nikita <zavalein@gmail.com>
2021-12-27 18:02:32 +0300

    add new functions

--- arguments/plushFaucetArgs.ts
+++ arguments/plushFaucetArgs.ts
@@ -1,3 +1,4 @@
 export default [
-  '0x8663f80619Cbc4562FF8e0986917429E917C79ba', // Plush Coin address
+  '0xE50865b4aa2fFe57a0cf4ADb64802Ea28426bc72', // Plush Coin address
+  '0x1c6f67D808Bf60EF0a8C8E8A6972f664cEDb85D6' // Plush Core Token address
 ];
--- contracts/PlushFaucet.sol
+++ contracts/PlushFaucet.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.2;
 
+import "./PlushCoreToken.sol";
+
 interface IERC20
 {
     function balanceOf(address account) external view returns (uint256);
@@ -11,17 +13,27 @@ interface IERC20
 
 contract PlushFaucet {
     IERC20 token;
+    PlushCoreToken plushCoreToken;
+
     address owner;
     mapping(address=>uint256) nextRequestAt;
+    mapping(address=>uint256) generalAmount;
     uint256 faucetDripAmount;
     uint256 faucetTime;
+    uint256 threshold;
+    bool isActive;
+    bool tokenNFTCheck;
 
-    constructor (address _smtAddress)
+    constructor (address _smtAddress, address _plushCoreToken)
     {
         token = IERC20(_smtAddress);
+        plushCoreToken = PlushCoreToken(_plushCoreToken);
         faucetTime = 24 hours;
         faucetDripAmount = 1 * 10 ** token.decimals();
+        threshold = 100 * 10 ** token.decimals();
         owner = msg.sender;
+        isActive = true;
+        tokenNFTCheck = true;
     }
 
     modifier onlyOwner
@@ -32,11 +44,18 @@ contract PlushFaucet {
 
     function send(address _receiver) external
     {
-        require(token.balanceOf(address(this)) > 1, "FaucetError: Empty");
+        require(isActive == true, "FaucetError: Faucet is not active");
+        require(token.balanceOf(address(this)) >= faucetDripAmount, "FaucetError: Empty");
         require(nextRequestAt[_receiver] < block.timestamp, "FaucetError: Try again later");
+        require(generalAmount[_receiver] < threshold, "FaucetError: You have exceeded the maximum number of coins");
+
+        if(tokenNFTCheck){
+            require(plushCoreToken.balanceOf(_receiver) > 0, "FaucetError: You don't have NTF(Plush Core Token) to get reward");
+        }
 
         // Next request from the address can be made only after faucetTime
         nextRequestAt[_receiver] = block.timestamp + faucetTime;
+        generalAmount[_receiver] += faucetDripAmount;
 
         token.transfer(_receiver, faucetDripAmount);
     }
@@ -51,14 +70,82 @@ contract PlushFaucet {
         faucetDripAmount = _amount;
     }
 
+    function setThreshold(uint256 _amount) external onlyOwner
+    {
+        threshold = _amount;
+    }
+
     function setFaucetTime(uint256 _time) external onlyOwner
     {
         faucetTime = _time;
+    }
+
+    function setFaucetActive(bool _isActive) external onlyOwner
+    {
+        isActive = _isActive;
+    }
+
+    function setTokenNFTCheck(bool _isCheck) external onlyOwner
+    {
+        tokenNFTCheck = _isCheck;
     }
 
     function withdrawTokens(address _receiver, uint256 _amount) external onlyOwner
     {
         require(token.balanceOf(address(this)) >= _amount, "FaucetError: Insufficient funds");
         token.transfer(_receiver, _amount);
+    }
+
+    function getThreshold() external view returns(uint256)
+    {
+        return threshold;
+    }
+
+    function getFaucetDripAmount() external view returns(uint256)
+    {
+        return faucetDripAmount;
+    }
+
+    function getFaucetBalance() external view returns(uint256)
+    {
+        return token.balanceOf(address(this));
+    }
+
+    function getDistributionTime() external view returns(uint256)
+    {
+        return faucetTime;
+    }
+
+    function getIsFaucetActive() external view returns(bool)
+    {
+        return isActive;
+    }
+
+    function getIsTokenNFTCheck() external view returns(bool)
+    {
+        return tokenNFTCheck;
+    }
+
+    function getDistributionOfAddress(address _receiver) external view returns(uint256)
+    {
+        if(nextRequestAt[_receiver] <= block.timestamp || nextRequestAt[_receiver] == 0){
+            return 0;
+        }
+
+        return nextRequestAt[_receiver] - block.timestamp;
+    }
+
+    function getCanTheAddressReceiveReward(address _receiver) external view returns(bool)
+    {
+        require(isActive == true, "Faucet is not active");
+        require(token.balanceOf(address(this)) >= faucetDripAmount, "Faucet is empty");
+        require(nextRequestAt[_receiver] < block.timestamp, "You received recently, try again later");
+        require(generalAmount[_receiver] < threshold, "You have exceeded the maximum number of tokens");
+
+        if(tokenNFTCheck){
+            require(plushCoreToken.balanceOf(_receiver) > 0, "FaucetError: You don't have NTF(Plush Core Token) to get reward");
+        }
+
+        return true;
     }
 }
