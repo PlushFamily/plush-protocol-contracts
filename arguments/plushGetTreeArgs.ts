36d8e626ed8be8cabae74e1dd97706ce9fe01148
Dmitrii <d.mingalev99@gmail.com>
2021-12-28 04:29:28 +0300

    update dev contract addresses

--- arguments/plushGetTreeArgs.ts
+++ arguments/plushGetTreeArgs.ts
@@ -1,5 +1,5 @@
 export default [
-  '0xd5015643F38A06b2962283Ec0B9fF555812Adcd0', // Forest address
-  '0x8663f80619Cbc4562FF8e0986917429E917C79ba', // Plush Coin address
+  '0x23cFE1bC1B940cBb6D5AdcC35846151d4446723F', // Forest address
+  '0x6ae301722a920905b9B404dCC4bB53135ADc4bD6', // Plush Coin address
   '0x5d367Ba836ce0C90E9fd3D58E0A7aCb6f63Dc7b1', // withdrawal address
 ];
