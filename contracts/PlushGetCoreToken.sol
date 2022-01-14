28a4e35d9b8960299fac232c1234032d1efd7a50
Dmitrii <d.mingalev99@gmail.com>
2022-01-14 13:52:05 +0300

    fixes

--- contracts/PlushGetCoreToken.sol
+++ contracts/PlushGetCoreToken.sol
@@ -5,14 +5,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./PlushCoreToken.sol";
 
 /// @custom:security-contact security@plush.family
-
-
 contract PlushGetCoreToken is Ownable {
 
-    event coreTokenChecked(
+    event coreTokenChecked
+    (
         address _holder,
         uint _bal,
-        bool _result);
+        bool _result
+    );
 
     PlushCoreToken plushCoreToken;
     address  payable safeAddress;
@@ -29,57 +29,70 @@ contract PlushGetCoreToken is Ownable {
         isActive = true;
     }
 
-    function checkToken(address _address) private returns (bool){
-        bool result;
+    function checkUserCoreToken(address _address) private returns (bool)
+    {
+        bool result = false;
+
         if (plushCoreToken.balanceOf(_address) > 0) {
             result = true;
-        } else {
-            result = false;
         }
+
         emit coreTokenChecked(_address, plushCoreToken.balanceOf(_address), result);
+
         return result;
     }
 
-    function pause() public onlyOwner {
+    function pause() public onlyOwner
+    {
         isActive = !isActive;
     }
 
-    function changeTokenCheckStatus() public onlyOwner {
+    function changeTokenCheckStatus() public onlyOwner
+    {
         require(isActive, "Contract is not active");
+
         tokenNFTCheck = !tokenNFTCheck;
     }
 
-    function changeMintPrice(uint256 _price) public onlyOwner {
+    function changeMintPrice(uint256 _price) public onlyOwner
+    {
         require(isActive, "Contract is not active");
+
         mintPrice = _price;
     }
 
-    function getMintPrice() external view returns (uint256) {
+    function getMintPrice() external view returns (uint256)
+    {
         return mintPrice;
     }
 
-    function setSafeAddress(address _address) external onlyOwner {
+    function setSafeAddress(address _address) external onlyOwner
+    {
         safeAddress = payable(_address);
     }
 
-    function setCoreTokenAddress(address _address) external onlyOwner {
+    function setCoreTokenAddress(address _address) external onlyOwner
+    {
         plushCoreToken = PlushCoreToken(_address);
     }
 
-    function getSafeAddress() external view onlyOwner returns (address) {
+    function getSafeAddress() external view onlyOwner returns (address)
+    {
         return safeAddress;
     }
 
-    function getCoreTokenAddress() external view onlyOwner returns (address) {
+    function getCoreTokenAddress() external view onlyOwner returns (address)
+    {
         return address(plushCoreToken);
     }
 
-    function mint(address _mintAddress) public payable {
+    function mint(address _mintAddress) public payable
+    {
         require(isActive, "Contract is not active");
         require(uint256(msg.value) == mintPrice, "Incorrect amount");
 
         if (tokenNFTCheck) {
-            require(checkToken(_mintAddress) == false, "You already have a Core token");
+            require(checkUserCoreToken(_mintAddress) == false, "You already have a Core token");
         }
 
         safeAddress.transfer(msg.value);
