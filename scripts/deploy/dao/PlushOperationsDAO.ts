55e4cac52a678b105376b6ea47a550775acac0ca
AlexBykovski <v.belokurski@gmail.com>
2022-03-17 15:59:11 +0300

    add proxy

--- scripts/deploy/dao/PlushOperationsDAO.ts
+++ scripts/deploy/dao/PlushOperationsDAO.ts
@@ -1,11 +1,20 @@
-import hre from 'hardhat';
+import hre, {upgrades} from 'hardhat';
 
 async function main() {
   const PlushOperationsDAO = await hre.ethers.getContractFactory(
     'PlushOperationsDAO',
   );
+  const plushOperationsDAO = await upgrades.deployProxy(
+    PlushOperationsDAO,
+    [
+      "0xEaCb79ef70A5FFD4698Aa5603054e8a914cFcf12", //erc20
+      "0x080190e04768F3E9939D3fa2865478b047d8F9fe", //timeLock
+    ],
+    {
+      kind: 'uups',
+    }
+  );
 
-  const plushOperationsDAO = await PlushOperationsDAO.deploy();
   await plushOperationsDAO.deployed();
 
   console.log(
@@ -23,7 +32,9 @@ async function main() {
     console.log('Verifying...\n');
 
     await hre.run('verify:verify', {
-      address: plushOperationsDAO.address,
+      address: await upgrades.erc1967.getImplementationAddress(
+          plushOperationsDAO.address,
+      ),
     });
   }
 }
