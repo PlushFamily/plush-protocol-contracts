683b4bb0a14af09a56e9e3dfcd5c91ffd8be733c
Maxirus2 <maximuskep@gmail.com>
2022-03-18 17:27:39 +0300

    Mg/dao proxy (#8)
    * add TimelockControllerUpgradeable deploy
    
    Co-authored-by: AlexBykovski <v.belokurski@gmail.com>
    Co-authored-by: Dmitrii <d.mingalev99@gmail.com>

---
+++ arguments/plushOperationsDAO.ts
@@ -0,0 +1,3 @@
+export default [
+  '0xF8923D6214150A71d9EDA0946e5ae1086Ba3f141', // Plush Coin address
+];
--- scripts/deploy/dao/PlushOperationsDAO.ts
+++ scripts/deploy/dao/PlushOperationsDAO.ts
@@ -1,29 +1,98 @@
-import hre from 'hardhat';
+import hre, { upgrades, ethers } from 'hardhat';
+import web3 from 'web3';
+import * as args from '../../../arguments/plushOperationsDAO';
+
+const MINDELAY = 60 * 60 * 24 * 3;
+const ZERO_ADDRESS = web3.utils.padLeft(0, 40);
+const PROPOSER_ROLE = ethers.utils.keccak256(
+  ethers.utils.toUtf8Bytes('PROPOSER_ROLE'),
+);
+const TIMELOCK_ADMIN_ROLE = ethers.utils.keccak256(
+  ethers.utils.toUtf8Bytes('TIMELOCK_ADMIN_ROLE'),
+);
 
 async function main() {
+  const [unlockOwner] = await ethers.getSigners();
+
+  const PlushTimeLock = await hre.ethers.getContractFactory('PlushTimeLock');
+
+  const plushTimeLock = await upgrades.deployProxy(PlushTimeLock, [
+    MINDELAY,
+    [], // proposers list is empty at deployment
+    [ZERO_ADDRESS], // allow any address to execute a proposal once the timelock has expired
+  ]);
+
+  await plushTimeLock.deployed();
+
+  console.log('> PlushTimeLock -> deployed to address:', plushTimeLock.address);
+
   const PlushOperationsDAO = await hre.ethers.getContractFactory(
     'PlushOperationsDAO',
   );
 
-  const plushOperationsDAO = await PlushOperationsDAO.deploy();
+  // deploy plushOperationsDAO proxy
+  const plushOperationsDAO = await upgrades.deployProxy(
+    PlushOperationsDAO,
+    [args.default[0], plushTimeLock.address],
+    { kind: 'uups' },
+  );
+
   await plushOperationsDAO.deployed();
 
   console.log(
-    'PlushOperationsDAO -> deployed to address:',
+    '> PlushOperationsDAO -> deployed to address:',
     plushOperationsDAO.address,
   );
 
+  // plushOperationsDAO should be the only proposer
+  await plushTimeLock.grantRole(PROPOSER_ROLE, plushOperationsDAO.address);
+
+  await new Promise(function (resolve) {
+    setTimeout(resolve, 10000);
+  });
+
+  console.log(
+    '> PlushOperationsDAO added to Timelock as sole proposer. ',
+    `${plushOperationsDAO.address} is Proposer: ${await plushTimeLock.hasRole(
+      PROPOSER_ROLE,
+      plushOperationsDAO.address,
+    )} `,
+  );
+
+  // deployer should renounced the Admin role after setup (leaving only Timelock as Admin)
+  await plushTimeLock.renounceRole(TIMELOCK_ADMIN_ROLE, unlockOwner.address);
+
+  await new Promise(function (resolve) {
+    setTimeout(resolve, 10000);
+  });
+
+  console.log(
+    '> Plush Owner recounced Admin Role. ',
+    `${unlockOwner.address} isAdmin: ${await plushTimeLock.hasRole(
+      TIMELOCK_ADMIN_ROLE,
+      unlockOwner.address,
+    )} `,
+  );
+
   if (process.env.NETWORK != 'local') {
-    console.log('Waiting 1m before verify contract\n');
+    console.log('> Waiting 1m before verify contracts\n');
 
     await new Promise(function (resolve) {
       setTimeout(resolve, 60000);
     });
 
-    console.log('Verifying...\n');
+    console.log('> Verifying...\n');
 
     await hre.run('verify:verify', {
-      address: plushOperationsDAO.address,
+      address: await upgrades.erc1967.getImplementationAddress(
+        plushTimeLock.address,
+      ),
+    });
+
+    await hre.run('verify:verify', {
+      address: await upgrades.erc1967.getImplementationAddress(
+        plushOperationsDAO.address,
+      ),
     });
   }
 }
