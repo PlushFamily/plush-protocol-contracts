6c295e0946411c78a5cd9d7f71b45a6cb50ac5b8
AlexBykovski <v.belokurski@gmail.com>
2022-04-06 16:09:57 +0300

    small fix

--- contracts/governance/PlushTimeLock.sol
+++ contracts/governance/PlushTimeLock.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
 contract PlushTimeLock is Initializable, TimelockControllerUpgradeable, UUPSUpgradeable, OwnableUpgradeable  {
   /// @custom:oz-upgrades-unsafe-allow constructor
