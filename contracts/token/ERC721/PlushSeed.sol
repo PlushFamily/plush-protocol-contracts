43bbbca5a2fb2a44405c087a3eb553099bbd4168
Dmitrii <d.mingalev99@gmail.com>
2022-07-04 14:30:23 +0300

    Switch PlushSeed on Ownable

--- contracts/token/ERC721/PlushSeed.sol
+++ contracts/token/ERC721/PlushSeed.sol
@@ -5,18 +5,16 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
 /// @custom:security-contact security@plush.family
-contract PlushSeed is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, ERC721BurnableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+contract PlushSeed is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, ERC721BurnableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     CountersUpgradeable.Counter private _tokenIdCounter;
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -28,15 +26,11 @@ contract PlushSeed is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradea
         __ERC721Enumerable_init();
         __ERC721URIStorage_init();
         __ERC721Burnable_init();
-        __AccessControl_init();
+        __Ownable_init();
         __UUPSUpgradeable_init();
-
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
-        _grantRole(UPGRADER_ROLE, msg.sender);
     }
 
-    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
@@ -45,7 +39,7 @@ contract PlushSeed is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradea
 
     function _authorizeUpgrade(address newImplementation)
     internal
-    onlyRole(UPGRADER_ROLE)
+    onlyOwner
     override
     {}
 
@@ -77,7 +71,7 @@ contract PlushSeed is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradea
     function supportsInterface(bytes4 interfaceId)
     public
     view
-    override(ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable)
+    override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
     returns (bool)
     {
         return super.supportsInterface(interfaceId);
--- scripts/deploy/token/ERC721/PlushSeed.ts
+++ scripts/deploy/token/ERC721/PlushSeed.ts
@@ -1,14 +1,6 @@
-import { ethers, upgrades, run } from 'hardhat';
-import { constants } from 'ethers';
+import { ethers, run, upgrades } from 'hardhat';
 
 import { DevContractsAddresses } from '../../../../arguments/development/consts';
-
-const MINTER_ROLE = ethers.utils.keccak256(
-  ethers.utils.toUtf8Bytes('MINTER_ROLE'),
-);
-const UPGRADER_ROLE = ethers.utils.keccak256(
-  ethers.utils.toUtf8Bytes('UPGRADER_ROLE'),
-);
 
 async function main() {
   const PlushSeed = await ethers.getContractFactory('PlushSeed');
@@ -16,56 +8,16 @@ async function main() {
     kind: 'uups',
   });
 
-  const signers = await ethers.getSigners();
-
   await plushSeed.deployed();
   console.log('PlushSeed -> deployed to address:', plushSeed.address);
 
-  console.log('Grant all roles for DAO Protocol...\n');
+  console.log('Switch contract owner...\n');
 
-  const grantAdminRole = await plushSeed.grantRole(
-    constants.HashZero,
+  const changeOwner = await plushSeed.transferOwnership(
     DevContractsAddresses.PLUSH_DAO_PROTOCOL_ADDRESS,
-  ); // ADMIN role
+  );
 
-  await grantAdminRole.wait();
-
-  const grantMinterRole = await plushSeed.grantRole(
-    MINTER_ROLE,
-    DevContractsAddresses.PLUSH_DAO_PROTOCOL_ADDRESS,
-  ); // MINTER role
-
-  await grantMinterRole.wait();
-
-  const grantUpgraderRole = await plushSeed.grantRole(
-    UPGRADER_ROLE,
-    DevContractsAddresses.PLUSH_DAO_PROTOCOL_ADDRESS,
-  ); // UPGRADER role
-
-  await grantUpgraderRole.wait();
-
-  console.log('Revoke all roles from existing account...\n');
-
-  const revokeMinterRole = await plushSeed.revokeRole(
-    MINTER_ROLE,
-    await signers[0].getAddress(),
-  ); // MINTER role
-
-  await revokeMinterRole.wait();
-
-  const revokeUpgraderRole = await plushSeed.revokeRole(
-    UPGRADER_ROLE,
-    await signers[0].getAddress(),
-  ); // UPGRADER role
-
-  await revokeUpgraderRole.wait();
-
-  const revokeAdminRole = await plushSeed.revokeRole(
-    constants.HashZero,
-    await signers[0].getAddress(),
-  ); // ADMIN role
-
-  await revokeAdminRole.wait();
+  await changeOwner.wait();
 
   if (process.env.NETWORK != 'local') {
     console.log('Waiting 1m before verify contract\n');
